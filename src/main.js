e356d0700aefc12d9b6e10b019f9e9b2d569a6a6
likwotsing <gq_li3051@163.com>
2020-05-30 11:12:13 +0800

    安装插件

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
@@ -14,5 +15,6 @@ Vue.directive('focus', {
 Vue.prototype.$bus = new Vue()
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
